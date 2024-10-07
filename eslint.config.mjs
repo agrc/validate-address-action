e5edb79fbedb27ed1b66665f4f5fb8f3d48d54e7
stdavis <stdavis@utah.gov>
2024-10-07 14:44:28 -0600

    deps: FY25Q2 dependency updates 🌲
    Includes upgrade to eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '.github/',
+      'package.json',
+      'package-lock.json',
+      '.emulator-data/',
+      '.husky/',
+      '.vscode/',
+      'coverage/',
+      'firebase-export-*/',
+      'public/assets/**',
+    ],
+  },
+);
