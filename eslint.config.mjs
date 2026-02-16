8466b27cb33fc5fc13e6bac4c1816b933a2bcf6d
stdavis <stdavis@utah.gov>
2026-02-16 16:07:39 -0700

    Revert "refactor: migrate to rollup and ugrc packages"
    This reverts commit db852de1106db0cae4a4b43da54e929acf966ab7.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,35 @@
-import { server } from '@ugrc/eslint-config';
+// @ts-check
 
-export default server;
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '.github/',
+      'package.json',
+      'package-lock.json',
+      '.emulator-data/',
+      '.husky/',
+      '.vscode/',
+      'coverage/',
+      'firebase-export-*/',
+      'public/assets/**',
+    ],
+  },
+);
